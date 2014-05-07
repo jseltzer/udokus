47a4729c4e9cec32137185e3f289903a1e55d398
Josh Seltzer <seltz@gmail.com>
2014-05-07 18:05:01 -0400

    board and solver

---
+++ board.py
@@ -0,0 +1,79 @@
+class Board():
+    def __init__(self: 'Board', rows: list):
+        self.rows = rows
+
+    def __str__(self: 'Board', changes: list=[], div: bool=True) -> str:
+        """Return human-readable board format."""
+        colour = {'red':'\033[91m', 'yellow':'\033[93m', 'green':'\033[92m', 'end':'\033[0m'}
+        hr = ''
+        i = 0
+        for row in self.rows:
+            j = 0
+            for val in row:
+                if val == 0:
+                    hr += colour['red'] + '- ' + colour['end']
+                elif (j,i) == changes[-1]:
+                    hr += colour['green'] + str(val) + ' ' + colour['end']
+                elif (j,i) in changes:
+                    hr += colour['yellow'] + str(val) + ' ' + colour['end']
+                else:
+                    hr += str(val) + ' '
+                j += 1
+                if j == 3 or j == 6:
+                    hr += '| '
+            i += 1
+            if i == 3 or i == 6:
+                hr += '\n' + '-'*21
+            hr += '\n'
+        return hr[:-1]
+
+    def _is_well_defined(self: 'Board') -> bool:
+        """Check for proper row amount and lengths."""
+        return (len(self.rows) == 9 
+                and all([len(row) == 9 for row in self.rows])
+                and all([x<10 and x>-1 for x in self.get_vals(self.rows)])
+               )
+
+    def _is_legal(self: 'Board'):
+        return not (self.has_dupes(self.rows) or self.has_dupes(self.get_cols()) 
+                    or any([self.has_dupes([self.get_box(n)]) for n in range(9)]))
+        #what about boxes!
+
+    def has_dupes(self: 'Board', rows: [list, list, ]) -> bool:
+        """Return True if each row doesn't have any dupes (besides 0)."""
+        return any([list(filter(lambda x: row[x] != 0 and row[x] in row[:x] + row[x+1:], range(len(row)))) != [] for row in rows])
+
+    def in_row(self: 'Board', row: int, val: int) -> bool:
+        return val in self.rows[row]
+
+    def in_col(self: 'Board', col: int, val: int) -> bool:
+        return val in self.get_col(col)
+
+    def get_col(self: 'Board', col: int) -> list:
+        return [self.rows[n][col] for n in range(9)]
+
+    def get_cols(self: 'Board') -> [list, list, ]:
+        return [self.get_col(n) for n in range(9)]
+
+    def get_vals(self: 'Board', rows: [list, list, ]) -> list:
+        l = []
+        for row in self.rows:
+            [l.append(val) for val in row]
+        return l
+
+    """
+    Boxes:
+    [0][1][2]
+    [3][4][5]
+    [6][7][8]
+    """
+
+    def get_box(self: 'Board', id: int) -> list:
+        l = []
+        x = 3 * (id%3)
+        y = 3 * (id//3)
+        [l.extend(self.rows[n][x:x+3]) for n in range(y,y+3)]
+        return l
+
+    def coords_to_box_id(self: 'Board', coords: (int, int)) -> int:
+        return (coords[0]//3) + 3*(coords[1]//3)
---
+++ solver.py
@@ -0,0 +1,86 @@
+from board import Board
+
+dig = list(range(1,10))
+
+b = Board([dig[n:] + dig[:n] for n in range(9)])
+
+comp = Board([[1,2,3,4,5,6,7,8,9], [4,5,6,7,8,9,1,2,3], [7,8,9,1,2,3,4,5,6], [2,3,4,5,6,7,8,9,1], [5,6,7,8,9,1,2,3,4], [8,9,1,2,3,4,5,6,7],
+           [3,4,5,6,7,8,9,1,2], [6,7,8,9,1,2,3,4,5], [9,1,2,3,4,5,6,7,8]])
+
+not_comp = Board([[0,2,0,4,0,0,7,0,9], [0,5,0,7,0,0,1,2,0], [7,0,9,0,2,3,0,0,6], [2,0,4,0,6,0,8,0,1], [0,6,7,8,9,0,2,0,0], [0,9,0,2,0,4,0,6,0],
+           [0,0,0,0,7,8,0,1,0], [6,0,8,0,1,0,3,4,5], [0,0,0,0,0,0,6,0,0]])
+
+not_comp2 = Board([[6,0,0,5,0,9,0,8,0], [0,9,2,0,0,0,0,0,0], [3,0,0,0,4,1,0,0,0], [0,0,0,0,2,0,6,0,5], [2,0,1,0,7,0,8,0,3], [4,0,8,0,1,0,0,0,0],
+           [0,0,0,3,9,0,0,0,1], [0,0,0,0,0,0,2,6,0], [0,7,0,4,0,2,0,0,9]])
+
+not_comp3 = Board([[6,3,0,0,0,1,9,0,0], [9,0,4,0,5,6,1,3,0], [0,0,0,0,0,9,0,0,8], [1,0,0,0,8,0,7,0,9], [0,6,0,0,0,0,2,1,0], [2,0,5,1,0,0,0,0,0],
+           [0,5,0,0,0,0,4,0,0], [0,2,1,7,0,5,0,0,0], [8,0,6,0,1,4,0,2,0]])
+
+
+
+
+
+def get_blanks(b: 'Board'):
+    x, y = 0, 0
+    blanks = []
+    for row in b.rows:
+        for val in row:
+            if val == 0:
+                blanks.append((x,y))
+            x += 1
+        x = 0
+        y += 1
+    return blanks if blanks != [] else None
+
+
+#solving
+
+
+def solve(b: 'Board', anim: bool=False):
+    if get_blanks(b) is None:
+        raise Exception("board already solved!")
+    print(b.__str__(solve_helper(b, anim)))
+    print('Solved board is legal!' if b._is_legal() else 'uh-oh')
+
+def solve_helper(b: 'Board', anim: bool=False) -> list:
+    changes = []
+
+    blank_options = {}
+    for blank in get_blanks(b):
+        blank_options[blank] = list(range(1,10))
+
+    is_changing = True
+    #exhaust elim_options for all blanks
+    while is_changing:
+        is_changing = False
+        blanks = get_blanks(b)
+        if blanks is None:
+            break
+        for blank in blanks:
+            new_options = elim_options(b, blank, blank_options[blank])
+            #check if options have changed
+            if new_options != blank_options[blank]:
+                is_changing = True
+                blank_options[blank] = new_options                
+            #check for solved numbers
+            if len(blank_options[blank]) == 1:
+                is_changing = True
+                b.rows[blank[1]][blank[0]] = blank_options[blank][0]
+                changes.append(blank)
+                if anim:
+                    print(b.__str__(changes, True) + '\n')
+                continue
+    changes.append(None)
+    return changes
+
+
+
+def elim_options(b: 'Board', coords: (int, int), options: list) -> list:
+    # remove any options which are in the same row, col, or box as coords
+    options = list(filter(lambda x: x not in (b.get_col(coords[0])
+                                           + b.rows[coords[1]] 
+                                           + b.get_box(b.coords_to_box_id(coords))
+                                          ), options
+                      )
+               )
+    return options
